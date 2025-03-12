76f3942ee5707a7269a287b90193dba6738beb7b
GreyA <grey.altaer@gmail.com>
2025-03-11 18:13:14 -0700

    component build: MailboxForm.jsx

--- src/components/MailboxForm/MailboxForm.jsx
+++ src/components/MailboxForm/MailboxForm.jsx
@@ -1,7 +1,26 @@
 const MailboxForm = (props) => {
+
   return (
     <main>
       <h2>New Mailbox</h2>
+      <form action="">
+        <label htmlFor="">
+            Enter a Boxholder: 
+        </label>
+        <input
+            type="text"
+            name="" id=""
+        />
+
+        <label htmlFor="">
+            Select a Box Size: 
+        </label>
+        <select name="" id="">
+
+        </select>
+
+        <button>Submit</button>
+      </form>
     </main>
   );
 };
